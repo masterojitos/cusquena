db88936302249e61df8da9f6fa457a0aca570e7e
Ricardo Garcia <master.ojitos@gmail.com>
2014-09-29 23:20:52 -0500

    optimizando indice temporal

--- js/main.js
+++ js/main.js
@@ -25,4 +25,32 @@
 
 
 // User code
-
+var $this, $target;
+$(document).on("ready", function() {
+    $("ul.indice li").on("click", function(e) {
+        $(".section-formulario .border").removeClass('error');
+        $(".section-mesa-roja .elegir-amigos").removeClass('hidden');
+        $(".section-mesa-roja .colocar-nombre, .agregar-amigo, .eliminar-amigo").addClass('hidden');
+        $this = $(this);
+        $target = $(e.target);
+        if ($target.is("span")) {
+            if ($this.data('to') === "section-formulario" && $target.data("show") === "error") {
+                $(".section-formulario .border").addClass('error');
+            } else if ($this.data('to') === "section-mesa-roja") {
+                if ($target.data("show") === "agregar") {
+                    $(".section-mesa-roja .elegir-amigos").addClass('hidden');
+                    $(".agregar-amigo").removeClass('hidden');
+                } else if ($target.data("show") === "eliminar") {
+                    $(".section-mesa-roja .elegir-amigos").addClass('hidden');
+                    $(".eliminar-amigo").removeClass('hidden');
+                }
+            }
+        }
+        $("section:visible").hide();
+        $("section." + $this.data("to")).fadeIn();
+        if ($this.data('to') === "section-mesa-roja" && $this.hasClass('nombre')) {
+            $(".section-mesa-roja .elegir-amigos").addClass('hidden');
+            $(".section-mesa-roja .colocar-nombre").removeClass('hidden');
+        }
+    });
+});
