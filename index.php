be7cab8d27ac93e2a5347bd9b076dfbbdba7c039
Ricardo Garcia Rodriguez <master.ojitos@gmail.com>
2014-10-10 12:15:31 -0500

    cambio en section que gano

--- index.php
+++ index.php
@@ -280,11 +280,13 @@ if (isset($_REQUEST['signed_request'])) {
                     <article>
                         <img class="top-logo" src="img/mesa-roja-cusquena.png" alt="MESA ROJA" width="393" height="210" />
                         <span class="enunciado">¿QUÉ GANO?</span><br /><br />
-                        <span class="enunciado white">8 entradas para </span><span class="enunciado">OKTOBERFEST</span>
+                        <span class="enunciado white">Una mesa roja reservada</span><br />
+                        <span class="enunciado">en el OKTOBERFEST</span>
                         <ul class="premios">
-                            <li>Comida y Cerveza gratis en el Festival</li>
-                            <li>KIT Oktoberfest</li>
-                            <li>6 packs de LATAS ROJA</li>
+                            <li><span>&bull;</span><strong>8 entradas</strong> para el festival</li>
+                            <li><span>&bull;</span><strong>8 tickets</strong> de comida</li>
+                            <li><span>&bull;</span><strong>32 vasos</strong> de <strong>Cusqueña Roja</strong></li>
+                            <li><span>&bull;</span>kit <strong>Oktoberfest</strong></li>
                         </ul>
                     </article>
                     <footer>
